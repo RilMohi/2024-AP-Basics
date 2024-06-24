6dbbb29686e25d5271b915e39695c4230404937c
RilMohi <rilmohi@chbc.school.nz>
2024-06-24 15:03:57 +1200

    Variables and hi

---
+++ P_02_variables.py
@@ -0,0 +1,21 @@
+#code to learn about variables
+
+#vairables can be a string (note the speech marks)
+greeting = "hello world"
+
+#variables can be a number
+fav_num = 1089587
+num_string = "hi "
+#variables can be a boolean (true/false)
+love_programming = True
+
+print(greeting)
+print(fav_num)
+print(love_programming)
+
+#yhis varaiable has a problem!
+my_name = "miss_g"
+
+print(fav_num * 747460068)
+print (num_string * 89)
+print (num_string * 10)
