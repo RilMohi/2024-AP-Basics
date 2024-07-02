132f8d411ecf26dc7957ea52f9227260d669af10
RilMohi <rilmohi@chbc.school.nz>
2024-07-03 09:52:44 +1200

    area check calculator

---
+++ P_06_area_check_calc.py
@@ -0,0 +1,13 @@
+#ask the user for the width and height
+#(assume they put in valid data)
+width = float(input("width: "))
+height = float(input("height: "))
+
+#calculate the area / perimeter
+area = width * height
+perimeter = 2 * (width + height)
+
+#output the area and perimeter
+print()
+print(f"Perimeter: {perimeter} units")
+print(f"area: {area} square units")
